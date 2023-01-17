7ef11c5cde828e54ba929b28230b401f96f747f3
Maik <samara.maik@yandex.by>
2023-01-17 16:01:00 +0300

    add randow number

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1,10); //случайное целое ичисло от 1 до 9
+Console.WriteLine(numberA); // показывает первое рандомное число
+int numberB = new Random().Next(1,10); // случайное целле число от 1 до 9
+Console.WriteLine(numberB); // показывает второе рандомное число
+int result = numberA + numberB;
+Console.WriteLine(result);
