41e4ba8a82ad222542a737763e6628afb9abcde3
Maik <samara.maik@yandex.by>
2023-01-17 16:37:09 +0300

    add example giri

---
+++ Example006_giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int a= 1;
+int b= 3;
+int c= 5;
+int d= 8;
+int i= 2;
+
+int max = a;
+
+if (a>max) max=a; // сравниваем максимальное число с весом гири 
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (i>max) max=i;
+
+Console.Write("max=");
+Console.WriteLine(max);
